e8a79d0a7da5fcb7881b745ef6ff671769cff877
John Qi <guangshuqi@gmail.com>
2019-06-14 16:36:29 -0400

    changed 'jobs' app to 'users' app

--- LLsite/urls.py
+++ LLsite/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import jobs.views
+import users.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jobs/', include('jobs.urls'))
+    path('users/', include('users.urls'))
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from jobs.models import Job
+from users.models import Job
 
 
 # Create your views here.
 def homepage(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    return render(request, 'users/home.html', {'jobs':jobs})
 
 def about(request):
     return HttpResponse('<h1>Job About</h1>')
